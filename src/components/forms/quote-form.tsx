dcb3535b5574ebea973adccbfbd46d67282330e1
Williks102 <koffiw4@gmail.com>
2025-11-09 13:31:50 +0000

    Update quote-form.tsx

--- src/components/forms/quote-form.tsx
+++ src/components/forms/quote-form.tsx
@@ -150,7 +150,7 @@ export function QuoteForm() {
           />
         </div>
         <Button size="lg" type="submit" className="w-full md:w-auto" disabled={form.formState.isSubmitting}>
-          {form.formState.isSubmitting ? "Envoi..." : "Faire ma Reservation"}
+          {form.formState.isSubmitting ? "Envoi..." : "Faire ma Réservation"}
         </Button>
       </form>
     </Form>
