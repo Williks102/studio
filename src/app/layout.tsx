775a429484f0f5b3c6b607a23aed0dc5d0ee4154
Williks102 <koffiw4@gmail.com>
2025-11-09 23:39:34 +0000

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type {Metadata} from 'next';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster"
-import { FloatingWhatsAppButton } from "@/components/floating-whatsapp-button"
+import { FloatingWhatsAppButton } from "@/components/FloatingWhatsAppButton"
 
 export const metadata: Metadata = {
   title: 'Restaurant LeLof',
